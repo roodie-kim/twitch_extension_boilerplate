cf8e6c2c11d4f15c8a6bf85ea8535aa567863ed7
hkahn <rood_optical@naver.com>
2019-06-11 12:05:44 +0900

    vue extension

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+    root: true,
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    env: {
+        browser: true,
+    },
+    extends: [
+        // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+        // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+        'plugin:vue/essential',
+        // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+        'standard'
+    ],
+    // required to lint *.vue files
+    plugins: [
+        'vue'
+    ],
+    // add your custom rules here
+    rules: {
+        'indent': ['error', 4],
+        'object-curly-spacing': ['error', 'always'],
+        'semi': [2, 'never'],
+        'arrow-parens': [2, 'as-needed'],
+        'comma-dangle': ['error', {
+            'arrays': 'always-multiline',
+            'objects': 'always-multiline',
+            'imports': 'always-multiline',
+            'exports': 'always-multiline',
+            'functions': 'never'
+        }],
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    }
+}
---
+++ src/components/Panel.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Hello this is a panel alright</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Panel',
+}
+</script>
+
+<style scoped>
+
+</style>
